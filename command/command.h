0f36cb99b4e4895ec9d161c5960cc8547a5d501e
NakagamiYuta <guguru0014@gmail.com>
2020-12-13 21:59:55 +0900

    [add] Command構造体定義

--- command/command.h
+++ command/command.h
@@ -1,4 +1,16 @@
 #ifndef MY_COMMAND_H
 #define MY_COMMAND_H
 
+#include "../vector/vector.h"
+
+typedef struct Command Command;
+struct Command {
+    void *func;
+    Vector *argv;
+    int stdin;
+    int stdout;
+    int stderr;
+    int result;
+};
+
 #endif  // !MY_COMMAND_H
