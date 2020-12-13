a9337e0e3849d060102e7d0a534322dd9b2fac31
NakagamiYuta <guguru0014@gmail.com>
2020-12-13 19:45:36 +0900

    [add] ユーザ情報を取得する処理を書いた

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+#include <pwd.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 int main(void) {
-    printf("Hello World!\n");
+    // ユーザ情報取得
+    uid_t uid = getuid();
+    struct passwd *pw = getpwuid(uid);
+    printf("Uid: %d\n", uid);
+    printf("Name: %s\n", pw->pw_name);
+    printf("HomePath: %s\n", pw->pw_dir);
+
+    return 0;
 }
